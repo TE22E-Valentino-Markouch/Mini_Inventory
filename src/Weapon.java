a242e3268bac57b1ba1b665f2d2fa5dd6f0bf80d
Valentino Markouch <Valentino.Markouch@elev.ga.ntig.se>
2024-11-13 21:18:23 +0100

    Weapon

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,2 +1,11 @@
-public class Weapon {
+import java.util.Random;
+
+public class Weapon extends Item {
+    public int minDamage = 10;
+    public int maxDamage = 20;
+
+    public int attack() {
+        Random random = new Random();
+        return random.nextInt(maxDamage - minDamage) + minDamage;
+    }
 }
